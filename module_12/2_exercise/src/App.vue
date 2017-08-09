4e3ec356f862e79dccb4f0d56b6c552ed7f8a696
Jan Armbrust <jan.armbrust@db-n.com>
2017-08-09 10:58:47 +0200

    Learned a lot about directives and building my own custom directives. In the exercise I built a custom directive with a bit of the functionality of v-on

---
+++ module_12/2_exercise/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+      <h1>Directives Exercise</h1>
+      <button class="btn btn-primary"
+              v-custom-on:click="clicked">
+              Click Me
+      </button>
+      <hr>
+      <div style="width: 100px; height: 100px; background-color: lightgreen"
+           v-custom-on:mouseenter="mouseEnter"
+           v-custom-on:mouseleave="mouseLeave">
+           <p style="margin: 5px">{{ mouseEnteredOrLeftText }}</p>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        mouseEnteredOrLeftText: ''
+      }
+    },
+    directives: {
+      customOn: {
+        bind(el, binding, vnode) {
+          const type = binding.arg;
+          const fn = binding.value;
+          el.addEventListener(type, fn);
+        }
+      }
+    },
+    methods: {
+      clicked() {
+        alert('I was clicked');
+      },
+      mouseEnter() {
+        this.mouseEnteredOrLeftText = "Mouse entered rectangle";
+      },
+      mouseLeave() {
+        this.mouseEnteredOrLeftText = "Mouse leaved rectangle";
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
